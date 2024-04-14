e78cf57fcb856b5bc0446812117fc976043ed0a5
LYJ01X <124449953+LYJ01X@users.noreply.github.com>
2024-04-14 19:26:14 +0800

    ip.js

--- ip.js
+++ ip.js
@@ -1,4 +1,4 @@
-$httpClient.get('https://searchplugin.csdn.net/api/v1/ip/get?ip', function (error, response, data) {
+$httpClient.get('https://ip.useragentinfo.com/myip', function (error, response, data) {
   $done({
     title: '当前 IP 地址',
     content: data,
