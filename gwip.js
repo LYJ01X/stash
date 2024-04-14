0b907fdb31d4f6df6db1d52545d1bade1ae15e85
LYJ01X <124449953+LYJ01X@users.noreply.github.com>
2024-04-14 20:39:24 +0800

    Create gwip.js

---
+++ gwip.js
@@ -0,0 +1,14 @@
+
+$httpClient.get('http://ip-api.com/json?lang=zh-CN', function (error, response, data){
+	let jsonData = JSON.parse(data)
+	let country =jsonData.country 
+	let query =jsonData.query
+	;
+	
+$done({
+    title: '当前国外IP地址',
+    content: `${country}\n${query}`,
+    backgroundColor: '#663399',
+    icon: 'network',
+  })
+ })
