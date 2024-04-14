48605a5734573a7136b4de1a45be28aad68f2ca8
LYJ01X <124449953+LYJ01X@users.noreply.github.com>
2024-04-14 20:28:54 +0800

    gnip.js

--- ip.js
+++ ip.js
@@ -1,8 +1,15 @@
-$httpClient.get('https://ip.useragentinfo.com/myip', function (error, response, data) {
-  $done({
-    title: '当前 IP 地址',
-    content: data,
+
+$httpClient.get('https://searchplugin.csdn.net/api/v1/ip/get?ip', function (error, response, data){
+	let jsonData = JSON.parse(data)
+	let sj =jsonData.data 
+	let address =sj.address
+	let ip =sj.ip
+	;
+	
+$done({
+    title: '当前国内IP地址',
+    content: `${address}\n${ip}`,
     backgroundColor: '#663399',
     icon: 'network',
   })
-})
+ })
