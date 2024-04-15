e867f32c662256443d13590732f9b8499c628c7e
LYJ01X <124449953+LYJ01X@users.noreply.github.com>
2024-04-15 13:13:47 +0800

    Update gwip.js

--- gwip.js
+++ gwip.js
@@ -4,7 +4,6 @@ $httpClient.get('http://ip-api.com/json?lang=zh-CN', function (error, response, 
 	let country =jsonData.country 
 	let query =jsonData.query
 	;
-	
 $done({
     title: '当前国外IP地址',
     content: `${country}\n${query}`,
