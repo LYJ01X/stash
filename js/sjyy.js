e963f6070a1dba2702d125b71b06764c853440dd
LYJ01X <124449953+LYJ01X@users.noreply.github.com>
2024-04-16 01:30:33 +0800

    Create sjyy.js

---
+++ js/sjyy.js
@@ -0,0 +1,12 @@
+.get('https://api.no0a.cn/api/onenote/query', function (error, response, data){
+	let jsonData = JSON.parse(data)
+	let onenote =jsonData.onenote 
+	;
+	
+	$done({
+    title: '随机一言',
+    content: onenote,
+    backgroundColor: '#663399',
+    icon: 'network',
+  })
+ })
